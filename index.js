4ac1790c60cb274f0f05385508e7bc69155da74d
rmessett15 <rmessett15@gmail.com>
2023-04-14 13:40:09 -0500

    added error handling for text length on first user prompt, and added comments to index.js

--- index.js
+++ index.js
@@ -33,36 +33,52 @@ function writeToFile(fileName, answers) {
   });
 }
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message:
-        "What text would you like you logo to display? (Enter up to three characters)",
-      name: "text",
-    },
-    {
-      type: "input",
-      message:
-        "Choose text color (Enter color keyword OR a hexadecimal number)",
-      name: "textColor",
-    },
-    {
-      type: "list",
-      message: "What shape would you like the logo to render?",
-      choices: ["Triangle", "Square", "Circle"],
-      name: "shape",
-    },
-    {
-      type: "input",
-      message:
-        "Choose shapes color (Enter color keyword OR a hexadecimal number)",
-      name: "shapeBackgroundColor",
-    },
-  ])
-  .then((answers) => {
-    writeToFile("logo.svg", answers);
-  });
+function promptUser() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message:
+          "What text would you like you logo to display? (Enter up to three characters)",
+        name: "text",
+        //   validate: function(input) {
+        //     if(input > 3) {
+        //     return true
+        //     } else {
+        //       console.log(`\n"Must enter a value of no more than 3 characters"`);
+        // }
+        // }
+      },
+      {
+        type: "input",
+        message:
+          "Choose text color (Enter color keyword OR a hexadecimal number)",
+        name: "textColor",
+      },
+      {
+        type: "list",
+        message: "What shape would you like the logo to render?",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape",
+      },
+      {
+        type: "input",
+        message:
+          "Choose shapes color (Enter color keyword OR a hexadecimal number)",
+        name: "shapeBackgroundColor",
+      },
+    ])
+    .then((answers) => {
+      if (answers.text.length > 3) {
+        console.log("Must enter a value of no more than 3 characters");
+        promptUser();
+      } else {
+        writeToFile("logo.svg", answers);
+      }
+    });
+}
+
+promptUser();
 
 // Need error handling -> how many characters user can enter into prompt 1
 //      error handling for color options -> need to enter a color or hexadecimal value
